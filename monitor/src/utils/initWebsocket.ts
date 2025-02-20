4e2fd86527a1bb647fb7689dece1f4ab86c926b1
carson2222 <olcraftyt@gmail.com>
2025-02-20 22:12:50 +0100

    websocket close handling; updating api route to the new one

--- monitor/src/utils/initWebsocket.ts
+++ monitor/src/utils/initWebsocket.ts
@@ -1,21 +1,38 @@
 import WebSocket from "ws";
 import { handleWebsocketClose } from "./handleWebsocketClose";
 
-export function initWebsocket() {
-  const webSocket = new WebSocket("wss://api-ui.hyperliquid.xyz/ws");
-  handleWebsocketClose(webSocket);
+const RECONNECT_INTERVAL = 5000; // 5 seconds
 
-  webSocket.onopen = () => {
-    webSocket.send(
-      JSON.stringify({
-        method: "subscribe",
-        subscription: {
-          type: "explorerBlock",
-        },
-      })
-    );
-    console.log("Successfully sent subscription request to explorerBlock");
+export function initWebsocket() {
+  let webSocket: WebSocket;
+
+  const connect = () => {
+    webSocket = new WebSocket("wss://api-ui.hyperliquid.xyz/ws");
+    // handleWebsocketClose(webSocket);
+
+    webSocket.onopen = () => {
+      webSocket.send(
+        JSON.stringify({
+          method: "subscribe",
+          subscription: {
+            type: "explorerBlock",
+          },
+        })
+      );
+      console.log("Successfully sent subscription request to explorerBlock");
+    };
+
+    webSocket.onclose = () => {
+      console.log("WebSocket closed. Attempting to reconnect...");
+      setTimeout(connect, RECONNECT_INTERVAL);
+    };
+
+    webSocket.onerror = (error) => {
+      console.error("WebSocket error:", error);
+      webSocket.close();
+    };
   };
 
-  return webSocket;
+  connect();
+  return webSocket!;
 }
